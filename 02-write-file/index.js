8732cc46ad7365e831dd8678d2f7ed596dc27565
Grigorii Iavorskii <yavgan06@gmail.com>
2021-11-03 15:07:59 +0400

    feat: add solution write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+let path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+
+const process = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+console.log('\n Type something: \n');
+
+rl.on('line', (inputText) => {
+    if(inputText === 'exit') {
+      rl.close()
+  } else {
+      fs.appendFile(path.join(__dirname, '/some.txt'), inputText+'\n', (err) => {
+          if(err) throw err
+      })
+  }
+});
+
+rl.on('pause', () => {
+  console.log('\nGood bye\n');
+});
+
+process.on('SIGINT', () => {process.exit();});
