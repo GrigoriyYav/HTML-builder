c6d62d39b0e7f62e31415e936f3bad32e488c912
Grigorii Iavorskii <yavgan06@gmail.com>
2021-11-05 14:07:03 +0400

    feat: add solution merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+let path = require('path');
+
+const folderPath = path.join(__dirname, '/styles');
+
+fs.rmdir(path.join(__dirname, '/project-dist/bundle.css'),{ recursive: true }, (err) =>{ 
+    if (err) {
+        return console.error(err);
+    }  
+
+    fs.readdir(folderPath,{withFileTypes: true}, (err, files) => {
+        files.forEach((file) => {
+            if (file.isFile() && path.extname(file.name) === '.css' ){
+
+                let stream = new fs.ReadStream(path.join(__dirname, `/styles/${file.name}`), {encoding: 'utf-8'});
+    
+                stream.on('readable', function(){
+                    let data = stream.read();
+
+                    if(data != null){
+                        fs.appendFile(path.join(__dirname, '/project-dist/bundle.css'), data+'\n', (err) => {
+                            if(err) throw err
+                        }) 
+                    };
+
+                });
+
+            }
+        });
+    });
+    
+});
